66719065b1663477c943fb803abd331a5c74a490
Rangel <criptideozinho@gmail.com>
2026-01-15 22:30:59 -0300

    renomeia pasta untitled para ex01

---
+++ fundamentos/Ex01/untitled/src/CalcAreaRet.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CalcAreaRet {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double lado1, lado2;
+
+        System.out.print("Informe o tamanho do lado da largura do retângulo em cm: ");
+        lado1 = sc.nextDouble();
+
+        System.out.print("Informe o tamanho do lado da altura  do retângulo em cm: ");
+        lado2 = sc.nextDouble();
+
+        double area = lado1 * lado2;
+        System.out.println("A área tem: " + area + "cm");
+
+    }
+}
